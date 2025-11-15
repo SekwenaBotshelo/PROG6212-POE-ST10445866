3d41174fe483a12c70607054a27198cb877104c6
BotsheloSe <st10445866@imconnect.edu.za>
2025-11-15 14:24:01 +0200

    Initial Update to the Views Folder intergrating the HR and Authentication Layer, as well as the Program.cs

--- Program.cs
+++ Program.cs
@@ -3,18 +3,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add Session services
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Session timeout
+    options.Cookie.HttpOnly = true; // Make cookie accessible only by server
+    options.Cookie.IsEssential = true; // Make session cookie essential
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
+
+// Add Session middleware - MUST be after UseRouting and before UseAuthorization
+app.UseSession();
 
 app.UseAuthorization();
 
@@ -24,6 +36,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
